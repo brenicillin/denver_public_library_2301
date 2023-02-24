070c006125123df78df163a2bb0b18ef789c2f72
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-02-24 11:48:50 -0500

    add write method

--- lib/author.rb
+++ lib/author.rb
@@ -6,4 +6,15 @@ class Author
     @name = (hash[:first_name] + ' ' + hash[:last_name])
     @books = []
   end
+
+  def write(name, publication_date)
+    hash = Hash.new
+    hash[:author_first_name] = self.name.split[0]
+    hash[:author_last_name] = self.name.split[-1]
+    hash[:title] = name
+    hash[:publication_date] = publication_date
+    book = Book.new(hash)
+    @books << book
+    book
+  end
 end
