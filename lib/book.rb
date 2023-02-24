78b64afbcb9dd5d796197d1146201e0d451ba5ed
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-02-24 11:09:53 -0500

    add initialize and publication_year methods

--- lib/book.rb
+++ lib/book.rb
@@ -1,3 +1,16 @@
 class Book
+  attr_reader :title,
+              :author,
+              :publication_date
+
+  def initialize(hash)
+    @title = hash[:title]
+    @author = (hash[:author_first_name] + ' ' + hash[:author_last_name])
+    @publication_date = hash[:publication_date]
+  end
+
+  def publication_year
+    @publication_date.split[2].to_i
+  end
   
 end
