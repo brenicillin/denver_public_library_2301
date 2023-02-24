43aad5d0316130089a458f11e5694b61fb006da7
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-02-24 12:17:27 -0500

    add pub time for author tests

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -24,10 +24,32 @@ RSpec.describe Library do
       jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1846")
       villette = charlotte_bronte.write("Villette", "1853")
       library.add_author(charlotte_bronte)
-      require 'pry'; binding.pry
 
       expect(library.authors.length).to eq(1)
       expect(library.books.length).to eq(2)
+    end
+  end
+
+  describe '#publication_time_frame_for' do 
+    it 'can return time frames for specific author' do
+      library = Library.new('Denver Public Library')
+      charlotte_bronte = Author.new({
+        first_name: "Charlotte", 
+        last_name: "Bronte"
+        })
+      jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1846")
+      villette = charlotte_bronte.write("Villette", "1853")
+      robert_frost = Author.new({
+        first_name: "Robert",
+        last_name: "Frost"
+      })
+      fire_and_ice = robert_frost.write("Fire and Ice", "December 1920")
+      nothing_gold = robert_frost.write("Nothing Gold can Stay", "October 1923")
+      library.add_author(charlotte_bronte)
+      library.add_author(robert_frost)
+
+      expect(publication_time_frame_for(charlotte_bronte)).to be_a(Hash)
+      expect(publication_time_frame_for(robert_frost)).to be_a(Hash)
     end
   end
 end
