d921f0cd3de012bed7da685a6bb8ab464e66fd01
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-02-24 11:16:33 -0500

    add author initialize method

--- lib/author.rb
+++ lib/author.rb
@@ -1,3 +1,9 @@
 class Author
-  
+  attr_reader :name,
+              :books
+              
+  def initialize(hash)
+    @name = (hash[:first_name] + ' ' + hash[:last_name])
+    @books = []
+  end
 end
